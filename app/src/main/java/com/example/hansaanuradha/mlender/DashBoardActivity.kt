4e761b4df5769faf9a23d43be496dab7c73571ba
Hansa Anuradha <hansaanuradha@Hansas-MacBook-Pro.local>
2018-09-23 19:21:33 +0530

    add transaction layout

--- app/src/main/java/com/example/hansaanuradha/mlender/DashBoardActivity.kt
+++ app/src/main/java/com/example/hansaanuradha/mlender/DashBoardActivity.kt
@@ -7,7 +7,9 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_dash_board.*
 
 class DashBoardActivity : AppCompatActivity() {
 
@@ -20,6 +22,23 @@ class DashBoardActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.app_bar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        customerCardView.setOnClickListener {
+            val intent = Intent(this, CustomerActivity::class.java)
+            startActivity(intent)
+        }
+        updateCardView.setOnClickListener {
+            Toast.makeText(this@DashBoardActivity, "You clicked Update.", Toast.LENGTH_SHORT).show()
+        }
+        toDoCardView1.setOnClickListener {
+            Toast.makeText(this@DashBoardActivity, "You clicked todo 1.", Toast.LENGTH_SHORT).show()
+        }
+        toDoCardView2.setOnClickListener {
+            Toast.makeText(this@DashBoardActivity, "You clicked todo 2.", Toast.LENGTH_SHORT).show()
+        }
+        addTransactionCardView.setOnClickListener {
+            val intent = Intent(this, TransactionActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
